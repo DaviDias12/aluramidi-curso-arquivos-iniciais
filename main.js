fcb2fa1ca018adc23e3665e8c03655b0a35fe84a
Davi Santos <davirr1220@gmail.com>
2022-11-09 17:49:17 -0300

    finalizando

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-function tocaSom (idElementoAudio){
-  document.querySelector(idElementoAudio).play ();  
+function tocaSom (idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
-
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSom;
 
-let contador = 0 
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; 
 
-while (contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = function() {
-      tocaSom('#som_tecla_pom');
-    }
-    contador = contador + 1;
-    console.log(contador);
+  tecla.onclick = function () {
+      tocaSom(idAudio);
+  }
+
 }
